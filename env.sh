0b016bd3245cbcf8985d54baa2438e9d9f5b997a
Christopher Reichert <creichert07@gmail.com>
2014-01-06 19:42:23 -0600

    Update environment script.

--- env.sh
+++ env.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 export PYTHONPATH=$PWD/integrator:$PWD/phonion/torchat/src
+
+# Use this until a build layout has been finalised. Use qmake install targets.
+export LD_LIBRARY_PATH=$PWD/integrator:$PWD/mumble/debug
