69477f7c3303fa38322ad4c7b48f934a80e8c207
Christopher Reichert <creichert07@gmail.com>
2014-01-16 21:13:59 -0600

    Add QPA env vars to env.sh

--- env.sh
+++ env.sh
@@ -4,3 +4,7 @@ export PYTHONPATH=$PWD/integrator:$PWD/phonion/torchat/src
 
 # Use this until a build layout has been finalised. Use qmake install targets.
 export LD_LIBRARY_PATH=$PWD/integrator:$PWD/mumble/debug
+
+export QT_QPA_EGLFS_PHYSICAL_WIDTH=100
+       QT_QPA_EGLFS_PHYSICAL_HEIGHT=100
+       QT_QPA_EGLFS_DEPTH=32
